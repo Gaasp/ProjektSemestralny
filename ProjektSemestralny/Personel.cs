b3513765a890f0b827cb314cc2a30a19baa4b1c3
Mateusz Leszkiewicz <mateusz.leszkiewicz@microsoft.wsei.edu.pl>
2021-02-16 22:38:24 +0100

    Added classes

--- ProjektSemestralny/Personel.cs
+++ ProjektSemestralny/Personel.cs
@@ -8,5 +8,10 @@ namespace ProjektSemestralny
 {
     class Personel
     {
+        public int id_personelu { get; set; }
+        public string imie { get; set; }
+        public string nazwisko { get; set; }
+        public string stanowisko { get; set; }
+        public int telefon { get; set; }
     }
 }
